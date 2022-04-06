314b9a26b59f204e91780f52f1977fb2a489e21d
adav <30274421+adavradou@users.noreply.github.com>
2022-04-07 02:04:03 +0300

    Add comments on functions.

--- src/models/LSTM.py
+++ src/models/LSTM.py
@@ -3,7 +3,6 @@ The LSTM model.
 """
 
 import os
-import sys
 import tensorflow as tf 
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dropout, Embedding, Dense
@@ -32,8 +31,11 @@ class LSTM(BaseModel):
       self.vec_layer = None
 
  
+
     """
-    Data Pipeline Function using TF Dataset API
+    Creates a data pipeline using the TF Dataset API.
+    Input: data(tf.data), labels(tf.data), batch_size(int), is_trainins(bool)
+    Output: dataset(tf.dataset)
     """ 
     def create_data_pipeline(self, texts, labels, batch_size=32, is_training=True):
       # Convert the inputs to a Dataset.
@@ -47,6 +49,13 @@ class LSTM(BaseModel):
       # Return the dataset.
       return dataset
 
+
+
+    """
+    Splits the training data to train and validation using the train_test_split() 
+    function from sklearn.
+    Input: train_data(tf.data)
+    """ 
     def split_training_data(self, train_data):
         #Split training dataset into train and validation sets
         self.train_df, self.val_df = train_test_split(train_data, test_size=0.2)
@@ -56,15 +65,27 @@ class LSTM(BaseModel):
         self.val_dataset = self.create_data_pipeline(self.val_df.tweet, self.val_df.sentiment, batch_size=128, is_training=False)
 
 
+
+    """
+    Vectorizes the data using the Text Vectorization API from tf.keras 
+    (https://www.tensorflow.org/api_docs/python/tf/keras/layers/TextVectorization).
+    It is common to apply it, before feeding the data to the model, in order to 
+    map text features to integer sequences.
+    Input: num of maximum features(int), num of max length(int)
+    """ 
     def data_vectorization(self, max_features=75000, max_len=50):
       self.vec_layer = tf.keras.layers.experimental.preprocessing.TextVectorization(
         max_tokens=max_features, output_sequence_length=max_len)
       self.vec_layer.adapt(self.train_df.tweet.values)
 
 
+
+    """
+    Create the model architecture.
+    Output: the created model(Model)
+    """ 
     def build_model(self):    
       print(self.input_shape)  
-      print(self.vec_layer)
       words = Input(shape=self.input_shape, dtype=tf.string)
       vectors = self.vec_layer(words)
       embeddings = Embedding(input_dim=self.input_dim, output_dim=self.output_dim)(vectors)
@@ -79,10 +100,14 @@ class LSTM(BaseModel):
       print(self.model.summary())
 
       return self.model
+
+
       
-		
+    """
+    Performs the model training.
+    Output: the training history of the model
+    """ 		
     def train(self):
-        #pass
         batch_size = 1024
         epochs = 3
         steps_per_epoch = self.train_df.tweet.shape[0] // batch_size
@@ -95,8 +120,13 @@ class LSTM(BaseModel):
 		
 
         return model_history.history['loss'], model_history.history['val_loss']
+
+
 		
-		
+    """
+    Performs the model evaluation.
+    Output: the resulted predictions of the model
+    """ 				
     def evaluate(self):
         pass
 		#predictions = []
